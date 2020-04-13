96a4955ebe7450e70569ee875fd48cd504da11c8
Ian Clark <ianclark226@gmail.com>
2020-04-13 15:28:49 -0400

    4/13

---
+++ react-portfolio/src/components/Carousel.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import passgen from '../Images/passgen';
+import quiz from '../Images/quiz';
+import weather from '../Images/weather';
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+    super(props);
+    this.state = {
+        items: [
+            {
+                id: 0,
+                title: 'Password Generater',
+                subTitle: 'Random Process',
+                imgSrc: passgen,
+                link:'https://github.com/ianclark226/PasswordGenerator',
+                selected: false
+            },
+            {
+                id: 1,
+                title: 'Pokemon TCG Quiz',
+                subTitle: 'Know your cards',
+                imgSrc: passgen,
+                link:'https://github.com/ianclark226/PokemonTCGQuiz',
+                selected: false
+            },
+            {
+                id: 2,
+                title: 'Weather Dashboard',
+                subTitle: 'Hows the weather up there?',
+                imgSrc: passgen,
+                link:'https://github.com/ianclark226/WeatherAPI',
+                selected: false
+            }
+        ]
+
+    }
+    }
+
+    handleCardClick = (id, card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !==id ) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+
+        makeItems = (items) => {
+            return items.map(item => {
+                return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+            })
+
+        }
+
+
+    }
+
+
+render() {
+    return(
+        <p>works</p>
+    )
+}
+}
